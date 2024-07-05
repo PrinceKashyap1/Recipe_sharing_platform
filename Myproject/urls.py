75016ffce98541c3416f2b0f9213253ef31353ab
Prince kashyap <princekashyap42534@gmail.com>
2024-07-06 02:26:38 +0530

    First add

---
+++ Myproject/urls.py
@@ -0,0 +1,53 @@
+"""
+URL configuration for Myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from home.views import home, about, contact
+from receipe_pro.views import *
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns = [
+    path('admin/',admin.site.urls),
+    # path('Recipes/',Recipes,name ="Recipes"),
+    path('main/',main,name ="main"),
+    path('Login/', Login_page, name="Login"),
+    path('Logout/', Logout_page, name="Logout_page"),
+    path('',main,name="main"),
+    path('register/',Reg, name="reg"),
+    path('tandc/',tandc,name="tandc"),
+    path('about/',about,name="about"),
+    path('addrecipe/',addrecipe,name="addrecipe"),
+    path('contact/',contact,name="contact"),
+    path('about/',about,name="about"),
+    path('about1/',about1,name="about1"),
+
+    path('Recipe_detail/<int:pk>/',Recipe_detail,name="Recipe_detail"),
+    path('delete/<int:pk>/',delete,name="delete")
+    
+
+    
+
+    # path('Success-page/',Success_page,name="Success_page"),
+   
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
---
+++ receipe_pro/views.py
@@ -0,0 +1,160 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect
+from.models import *
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import render, get_object_or_404
+
+
+# Create your views here.
+def main(request):
+    return render(request,'main.html')
+
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
+def Login_page(request):
+    if request.method == "POST":
+            data = request.POST 
+            username = data.get('username')
+            password = data.get('password')
+            if not User.objects.filter(username = username).exists():
+                messages.error(request,'Invalid Username')
+                return redirect('/Login/')
+            else:
+                user = authenticate(username = username, password = password)
+                if user is None:
+                    print(user)
+                    messages.error(request,'Invalid Password')
+                    return redirect('/Login/')
+                else:
+                    login(request,user)
+                    return redirect('/main/')
+    
+    
+    return render(request,'Login.html')
+                 
+
+     
+
+def Reg(request):
+    if request.method == "POST":
+            data = request.POST 
+        
+            username = data.get('username')
+            email = data.get('email')
+            password = data.get('user_pass')
+
+            user = User.objects.filter(username = username)
+
+            if user.exists():
+                messages.info(request,'Username already Taken')
+                
+                return redirect('/register/')
+
+            user = User.objects.create(
+            
+            username = username,
+            email = email,
+            password = password
+            )
+
+            user.set_password(password)
+            user.save()
+            messages.info(request,'Account created successfully')
+            return redirect('/Login/')
+       
+    return render(request,'Reg.html')
+
+       
+@login_required
+def addrecipe(request):
+    
+    recipe_user_name = request.user.username
+    login_user = Recipe.objects.filter(recipe_user_name=recipe_user_name)
+
+
+    if request.method == "POST":
+        data = request.POST 
+        recipe_image = request.FILES.get('receipe_image')
+        recipe_name = data.get('receipe_name')
+        recipe_user_name = recipe_user_name
+        recipe_description = data.get('receipe_description')
+        recipe_detail_description = data.get('recipe_detail_description')
+        print(recipe_detail_description)
+    
+        Recipe.objects.create(
+            recipe_user_name = recipe_user_name,
+            recipe_name = recipe_name,
+            recipe_description = recipe_description,
+            recipe_detail_description = recipe_detail_description,
+            recipe_image = recipe_image
+        )
+
+        return redirect("/addrecipe/")
+
+    queryset = Recipe.objects.all()
+    context = {'recipe':queryset,
+            'user_data': login_user
+             }
+    
+    
+
+    return render(request, 'addrecipe.html',context)
+
+def delete(request,pk):
+    if request.method == "POST":
+        recipe = get_object_or_404(Recipe,pk=pk)
+
+        recipe.delete()
+        
+        return redirect('/addrecipe/')    
+        
+    return render(request,"addrecipe.html")
+
+def main(request):
+    data = Recipe.objects.all()
+    
+    if request.method == "GET":
+        Search = request.GET.get('Search')
+        
+        if Search!=None:
+            
+            data = Recipe.objects.filter(recipe_name__icontains=Search)#__icontains can work with similar like data matches the database
+    
+        
+
+    context = {'Recipes': data,
+               'Recipe_count': data.count(),
+                'Search_name': Search }
+    
+    
+    return render(request,"main.html",context)
+
+def tandc(request):
+    return render(request,"tandc.html")
+
+# @login_required
+def Logout_page(request):
+    logout(request)
+    return redirect('/main/')
+
+def about(request):
+
+    return render(request,"about.html")
+
+def about1(request):
+
+    return render(request,"about1.html")
+
+    
+
+@login_required
+def Recipe_detail(request,pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
