950b7b5d5fedbb2b30bd1e6f9f3666de5df23927
Prince kashyap <princekashyap42534@gmail.com>
2024-07-06 12:35:02 +0530

    change setings

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIR  = [
-    os.path.join(BASE_DIR,"static"),
+    os.path.join(BASE_DIR,'staticfiles_build',"static"),
 ]
 
 
